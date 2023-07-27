165dd70035a388018184598152345a65a754e9dd
Mugambi Javan <mugambijavan@gmail.com>
2023-07-27 12:05:36 +0300

    dataweb landing page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Analytics from './components/Analytics';
+
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+    
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
